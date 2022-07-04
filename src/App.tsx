7ab63aec55bb425d042dcae2930992fd13053c3d
zstipanovic <zvonimir@prototyp.digital>
2022-07-04 21:59:56 +0200

    removed all files

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import { Layout } from "modules";
-import { Configurator } from "views/configurator";
-
 function App() {
   return (
-    <Layout>
-      <Configurator />
-    </Layout>
+    <div>
+      <p>Hello</p>
+    </div>
   );
 }
 
--- src/modules/layout/components/layout/Layout.styles.ts
+++
@@ -1,6 +0,0 @@
-import { css } from '@emotion/react'
-
-export const wrapper = css`
-  margin: 0 auto;
-  max-width: 800px;
-`
